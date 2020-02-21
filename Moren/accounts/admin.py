6fac501e1c15a5277788544bdd0d22190e3decc2
jjmp-dsk <szymon.pitula@codeteam.pl>
2020-02-21 12:37:34 +0100

    Remove old type of role handling

--- Moren/accounts/admin.py
+++ Moren/accounts/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from movies.models import Genre, Movie, Rental
-from accounts.models import AppUser, Role
+from accounts.models import AppUser
 
 admin.site.register(Genre)
 admin.site.register(Movie)
 admin.site.register(Rental)
 admin.site.register(AppUser)
-admin.site.register(Role)
--- Moren/accounts/models.py
+++ Moren/accounts/models.py
@@ -2,27 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Role(models.Model):
-    CUSTOMER = 1
-    EMPLOYEE = 2
-    ADMIN = 3
-
-    ROLE_CHOICES = (
-        (CUSTOMER, 'customer'),
-        (EMPLOYEE, 'employee'),
-        (ADMIN, 'admin'),
-    )
-
-    id = models.PositiveSmallIntegerField(
-        choices=ROLE_CHOICES, primary_key=True)
-
-    def __str__(self):
-        return self.get_id_display()
-
-
 class AppUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    roles = models.ManyToManyField(Role)
 
     def __str__(self):
         return self.user.username
